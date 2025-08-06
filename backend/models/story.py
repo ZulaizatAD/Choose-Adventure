14ae512e5ebce479657d4ab851f9fb855aea9cad
Zulaizat <zulaizatad@gmail.com>
2025-08-06 16:34:41 +0800

    Backend Frame
    Completed backend framework

--- backend/models/story.py
+++ backend/models/story.py
@@ -1,11 +1,11 @@
 from xmlrpc.client import Boolean
 
 from psycopg2._psycopg import Column
-from sqlalchemy_import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
-from backend.db.database import Base
+from db.database import Base
 
 class Story(Base):
     __tablename__ = "stories"
